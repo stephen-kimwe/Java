8d47f8b127b2412594d3507f6c42d551649e00a3
Stephen Kimwe <65422846+stephen-kimwe@users.noreply.github.com>
2021-11-19 18:45:44 +0300

    Add files via upload

---
+++ Movies.java
@@ -0,0 +1,8 @@
+public class Movies
+{
+public static void main(String[] args){
+/*Singles*/
+System.out.println("Sanak, Dune, Blackphone, Halloween Kills");
+/*Series*/
+System.out.println("My Name, The Equalizer Season 1/2, Ganglands, In the Dark");}
+}
